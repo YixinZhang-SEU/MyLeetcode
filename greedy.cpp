c72de2028580609110702041f4b39b151c148874
YixinZhang-SEU <220212021@seu.edu.cn>
2022-10-04 11:01:30 +0800

    221004

---
+++ greedy.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    // 455. 分发饼干
+    // int findContentChildren(vector<int>& g, vector<int>& s) {
+    //     sort(g.begin(), g.end());
+    //     sort(s.begin(), s.end());
+    //     int idx = s.size()-1;
+    //     int res = 0;
+    //     for (int i = g.size()-1; i >= 0; i--) {
+    //         if (idx >= 0 && s[idx] >= g[i]) {
+    //             res++;
+    //             idx--;
+    //         }
+    //     }
+    //     return res;
+    // }
+};
+
+int main() {
+    Solution solution;
+
+    // 455. 分发饼干
+    // vector<int> g = {1, 2};
+    // vector<int> s = {1, 2, 3};
+    // int res = solution.findContentChildren(g, s);
+    // cout<<res;
+}
