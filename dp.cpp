88a333fc44e96e83f323028acf191497a3cabf4a
YixinZhang-SEU <220212021@seu.edu.cn>
2022-11-02 14:08:37 +0800

    221102

--- dp.cpp
+++ dp.cpp
@@ -133,6 +133,29 @@ public:
     //     }
     //     return dp[n];
     // }
+
+    // 416. 分割等和子集
+    // bool canPartition(vector<int>& nums) {
+    //     int sum = 0;
+    //     for (int num : nums) {
+    //         sum += num;
+    //     }
+    //     if (sum%2) {
+    //         return false;
+    //     }
+    //     int target = sum/2;
+    //     vector<int> dp(10001, 0);
+    //     dp[0] = 0;
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         for (int j = target; j >= nums[i]; j--) {
+    //             dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
+    //         }
+    //     }
+    //     if (dp[target] == target) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
 };
 
 int main() {
@@ -153,4 +176,8 @@ int main() {
 
     // 96. 不同的二叉搜索树
     // cout<<solution.numTrees(3);
+
+    // 416. 分割等和子集
+    // vector<int> nums = {1,2,3,5};
+    // cout<<solution.canPartition(nums);
 }
