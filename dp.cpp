b5336832ccfd736f3a02b96f2a885e6b678d3aca
YixinZhang-SEU <220212021@seu.edu.cn>
2023-02-25 16:33:11 +0800

    230225

--- dp.cpp
+++ dp.cpp
@@ -603,6 +603,20 @@ public:
     //     }
     //     return dp[nums1.size()][nums2.size()];
     // }
+
+    // 53. 最大子数组和
+    // int maxSubArray(vector<int>& nums) {
+    //     vector<int> dp(nums.size());
+    //     dp[0] = nums[0];
+    //     int res = dp[0];
+    //     for (int i = 1; i < nums.size(); i++) {
+    //         dp[i] = max(dp[i-1] + nums[i], nums[i]);
+    //         if (dp[i] > res) {
+    //             res = dp[i];
+    //         }
+    //     }
+    //     return res;
+    // }
 }
 ;
 
