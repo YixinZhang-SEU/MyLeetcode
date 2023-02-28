d71f7a58ae1554b28efab95b40c10a19071a2228
YixinZhang-SEU <220212021@seu.edu.cn>
2023-02-28 11:18:22 +0800

    230228

--- dp.cpp
+++ dp.cpp
@@ -690,6 +690,28 @@ public:
     //     }
     //     return len1 + len2 - dp[len1-1][len2-1]*2;
     // }
+
+    // 72. 编辑距离
+    // int minDistance(string word1, string word2) {
+    //     int len1 = word1.size(), len2 = word2.size();
+    //     vector<vector<int> > dp(len1+1, vector<int>(len2+1, 0));
+    //     for (int j = 1; j <= len2; j++) {
+    //         dp[0][j] = j;
+    //     }
+    //     for (int i = 1; i <= len1; i++) {
+    //         dp[i][0] = i;
+    //     }
+    //     for (int i = 1; i <= len1; i++) {
+    //         for (int j = 1; j <= len2; j++) {
+    //             if (word1[i-1] == word2[j-1]) {
+    //                 dp[i][j] = dp[i-1][j-1];
+    //             } else {
+    //                 dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1;
+    //             }
+    //         }
+    //     }
+    //     return dp[len1][len2];
+    // }
 }
 ;
 
@@ -783,4 +805,9 @@ int main() {
     // string s = "xslledayhxhadmctrliaxqpokyezcfhzaskeykchkmhpyjipxtsuljkwkovmvelvwxzwieeuqnjozrfwmzsylcwvsthnxujvrkszqwtglewkycikdaiocglwzukwovsghkhyidevhbgffoqkpabthmqihcfxxzdejletqjoxmwftlxfcxgxgvpperwbqvhxgsbbkmphyomtbjzdjhcrcsggleiczpbfjcgtpycpmrjnckslrwduqlccqmgrdhxolfjafmsrfdghnatexyanldrdpxvvgujsztuffoymrfteholgonuaqndinadtumnuhkboyzaqguwqijwxxszngextfcozpetyownmyneehdwqmtpjloztswmzzdzqhuoxrblppqvyvsqhnhryvqsqogpnlqfulurexdtovqpqkfxxnqykgscxaskmksivoazlducanrqxynxlgvwonalpsyddqmaemcrrwvrjmjjnygyebwtqxehrclwsxzylbqexnxjcgspeynlbmetlkacnnbhmaizbadynajpibepbuacggxrqavfnwpcwxbzxfymhjcslghmajrirqzjqxpgtgisfjreqrqabssobbadmtmdknmakdigjqyqcruujlwmfoagrckdwyiglviyyrekjealvvigiesnvuumxgsveadrxlpwetioxibtdjblowblqvzpbrmhupyrdophjxvhgzclidzybajuxllacyhyphssvhcffxonysahvzhzbttyeeyiefhunbokiqrpqfcoxdxvefugapeevdoakxwzykmhbdytjbhigffkmbqmqxsoaiomgmmgwapzdosorcxxhejvgajyzdmzlcntqbapbpofdjtulstuzdrffafedufqwsknumcxbschdybosxkrabyfdejgyozwillcxpcaiehlelczioskqtptzaczobvyojdlyflilvwqgyrqmjaeepydrcchfyftjighntqzoo";
     // string t = "rwmimatmhydhbujebqehjprrwfkoebcxxqfktayaaeheys";
     // cout<<solution.numDistinct(s, t);
+
+    // 72. 编辑距离
+    // string word1 = "";
+    // string word2 = "a";
+    // cout<<solution.minDistance(word1, word2);
 }
