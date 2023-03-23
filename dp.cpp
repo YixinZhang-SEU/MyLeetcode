55fa522d146901426056f744055aaeec9e18899c
YixinZhang-SEU <220212021@seu.edu.cn>
2023-03-23 19:18:27 +0800

    230323

--- dp.cpp
+++ dp.cpp
@@ -803,6 +803,26 @@ public:
     //     }
     //     return dp[0][s.size()-1];
     // }
+
+    // 338. 比特位计数
+    // vector<int> countBits(int n) {
+    //     if (n == 0) {
+    //         return{0};
+    //     } 
+    //     if (n == 1) {
+    //         return{0, 1};
+    //     }
+    //     vector<int> dp(n+1, 0);
+    //     dp[0] = 0, dp[1] = 1;
+    //     for (int i = 2; i <= n; i++) {
+    //         if (i%2) {
+    //             dp[i] = dp[i-1] + 1;
+    //         } else {
+    //             dp[i] = dp[i/2];
+    //         }
+    //     }
+    //     return dp;
+    // }
 }
 ;
 
