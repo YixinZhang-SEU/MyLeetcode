86dbf1df23fb556bf269d1b0c715fd7af9cee50b
YixinZhang-SEU <220212021@seu.edu.cn>
2023-09-20 17:20:28 +0800

    230920

--- graphs.cpp
+++ graphs.cpp
@@ -139,52 +139,129 @@ public:
     // }
 
     // 1020. 飞地的数量
-    vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    // vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    // //
+    // void dfs(vector<vector<int>>& grid, vector<vector<int>>& isVisited, int x, int y) {
+    //     if (isVisited[x][y] || grid[x][y] == 0) {
+    //         return;
+    //     }
+    //     grid[x][y] = 0;
+    //     isVisited[x][y] = 1;
+    //     for (int i = 0; i < 4; i++) {
+    //         int nextx = x + directions[i][0];
+    //         int nexty = y + directions[i][1];
+    //         if (nextx < 0|| nextx >= grid.size() || nexty < 0 || nexty >= grid[0].size()) {
+    //             continue;
+    //         }
+    //         dfs(grid, isVisited, nextx, nexty);
+    //     }
+    // }
+    // //
+    // int numEnclaves(vector<vector<int>>& grid) {
+    //     int m = grid.size();
+    //     int n = grid[0].size();
+    //     vector<vector<int>> isVisited(m, vector<int>(n, 0));
+    //     //
+    //     for (int i = 0; i < m; i++) {
+    //         dfs(grid, isVisited, i, 0);
+    //         dfs(grid, isVisited, i, n - 1);
+    //     }
+    //     for (int j = 0; j < n; j++) {
+    //         dfs(grid, isVisited, 0, j);
+    //         dfs(grid, isVisited, m - 1, j);
+    //     }
+    //     //
+    //     int count = 0;
+    //     for (auto g : grid) {
+    //         for (auto n : g) {
+    //             if (n == 1) {
+    //                 count++;
+    //             }
+    //         }
+    //     }
+    //     return count;
+    // }
 
-    void dfs(vector<vector<int>>& grid, vector<vector<int>>& isVisited, int x, int y) {
-        if (isVisited[x][y] || grid[x][y] == 0) {
-            return;
-        }
-        grid[x][y] = 0;
-        isVisited[x][y] = 1;
-        for (int i = 0; i < 4; i++) {
-            int nextx = x + directions[i][0];
-            int nexty = y + directions[i][1];
-            if (nextx < 0|| nextx >= grid.size() || nexty < 0 || nexty >= grid[0].size()) {
-                continue;
-            }
-            dfs(grid, isVisited, nextx, nexty);
-        }
-    }
+    // 130. 被围绕的区域
+    // vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    // //
+    // void dfs(vector<vector<char>>& board, int x, int y) {
+    //     if (board[x][y] == 'X' || board[x][y] == 'V') {
+    //         return;
+    //     }
+    //     board[x][y] = 'V';
+    //     for (int i = 0; i < 4; i++) {
+    //         int nextx = x + directions[i][0];
+    //         int nexty = y + directions[i][1];
+    //         if (nextx < 0|| nextx >= board.size() || nexty < 0 || nexty >= board[0].size()) {
+    //             continue;
+    //         }
+    //         dfs(board, nextx, nexty);
+    //     }
+    // }
+    // //
+    // void solve(vector<vector<char>>& board) {
+    //     int m = board.size();
+    //     int n = board[0].size();
+    //     for (int i = 0; i < m; i++) {
+    //         dfs(board, i, 0);
+    //         dfs(board, i, n - 1);
+    //     }
+    //     for (int j = 0; j < n; j++) {
+    //         dfs(board, 0, j);
+    //         dfs(board, m - 1, j);
+    //     }
+    //     //
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             if (board[i][j] == 'V') {
+    //                 board[i][j] = 'O';
+    //             } else {
+    //                 board[i][j] = 'X';
+    //             }
+    //         }
+    //     }
+    // }
 
-    int numEnclaves(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> isVisited(m, vector<int>(n, 0));
-        
-        for (int i = 0; i < m; i++) {
-            dfs(grid, isVisited, i, 0);
-            dfs(grid, isVisited, i, n - 1);
-        }
-        for (int j = 0; j < n; j++) {
-            dfs(grid, isVisited, 0, j);
-            dfs(grid, isVisited, m - 1, j);
-        }
-
-        int count = 0;
-        for (auto g : grid) {
-            for (auto n : g) {
-                if (n == 1) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
+    // 1971. 寻找图中是否存在路径
+    // // 寻根
+    // int find (int u, vector<int>& father) {
+    //     return u == father[u] ? u : father[u] = find(father[u], father);
+    // }
+    // // 连接
+    // void join(int u, int v, vector<int>& father) {
+    //     u = find(u, father);
+    //     v = find(v, father);
+    //     if (u == v) {
+    //         return;
+    //     }
+    //     father[v] = u;
+    // }
+    // // 查询
+    // bool isSame(int u, int v, vector<int>& father) {
+    //     u = find(u, father);
+    //     v = find(v, father);
+    //     return u == v;
+    // }
+    // // 
+    // bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+    //     vector<int> father(n, 0);
+    //     // 初始化
+    //     for (int i = 0; i < n; i++) {
+    //         father[i] = i;
+    //     }
+    //     // 并
+    //     for (auto edge : edges) {
+    //         join(edge[0], edge[1], father);
+    //     }
+    //     // 查
+    //     return isSame(source, destination, father);
+    // }
 };
 
 int main() {
     Solution solution;
-    // 
-
+    // 130. 被围绕的区域
+    // vector<vector<char>> board = {{'O', 'O'}, {'O', 'O'}};
+    // solution.solve(board);
 }
